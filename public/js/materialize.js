6d399a3ef32c38bf61962a870d449f69140cb224
AlinaSM <alinasalmen995@gmail.com>
2019-05-21 09:04:38 -0500

    Agregando files

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -35,3 +35,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Tooltip.init(elems);
   });
 
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems);
+  });
